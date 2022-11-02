f4609bc90357d023529f6d5b66bbf2b0c135c876
Ignace Pierre <ipierre354@gmail.com>
2022-11-02 17:17:35 -0400

    Feat: Created all component files and added routes to the Apps.js file for frontend of project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,23 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Page from "./pages/Page/Page";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import Comment from "./components/Comment/Comment";
+import CommentForm from "./components/CommentForm/CommentForm";
+import CommentList from "./components/CommentList/CommentList";
+import RelatedVideos from "./components/RelatedVideos/RelatedVideos";
+import Reply from "./components/Reply/Reply";
+import ReplyForm from "./components/ReplyForm/ReplyForm";
+import ReplyList from "./components/ReplyList/ReplyList";
+import SearchBar from "./components/SearchBar/SearchBar";
+import SearchPage from "./components/SearchPage/SearchPage";
+import VideoPage from "./components/VideoPage/VideoPage";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
