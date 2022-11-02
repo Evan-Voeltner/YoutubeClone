ca8eb0b3d79896acabc9e5fcc520b9db77d44959
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-02 14:29:40 -0500

    fix: Corrected the replies URL path

--- backend/replies/urls.py
+++ backend/replies/urls.py
@@ -4,5 +4,5 @@ from replies import views
 
 urlpatterns = [
     path('id/<int:pk>/', views.replies_update),
-    path('/<int:pk>/', views.replies_update),
+    path('<int:pk>/', views.replies_update),
 ]
