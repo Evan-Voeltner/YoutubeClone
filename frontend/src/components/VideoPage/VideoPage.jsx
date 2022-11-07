4b80b8345a6d1d9658cc6597c6c171457578aa9c
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-07 11:49:55 -0600

    feat: Video selected from video page with route to video page with the selected video object

--- frontend/src/components/VideoPage/VideoPage.jsx
+++ frontend/src/components/VideoPage/VideoPage.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { Grid, Paper, Typography } from "@material-ui/core";
 
-const VideoPage = ({ video, onVideoSelect }) => {
+const VideoPage = (props) => {
+  console.log('Video in videoPage', props)
+  let videoSource = `https://www.youtube.com/embed/${props.currentVideo.id.videoId}`
+
+  
   return (
-    <Grid item xs={12}>
-      <Paper
-        style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-        onClick={() => onVideoSelect(video)}
-      >
-        <img
-          style={{ marginRight: "20px" }}
-          alt="thumbnail"
-          src={video.snippet.thumbnails.medium.url}
-        />
-        <Typography variant="subtitle1">
-          <b>{video.snippet.title}</b>
-        </Typography>
-      </Paper>
-    </Grid>
+    <div>
+      <iframe src={videoSource} frameBorder="0"></iframe>
+    </div>
+
+
+
+    // <Grid item xs={12}>
+    //   <Paper
+    //     style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+    //     onClick={() => onVideoSelect(video)}
+    //   >
+    //     <img
+    //       style={{ marginRight: "20px" }}
+    //       alt="thumbnail"
+    //       src={video.snippet.thumbnails.medium.url}
+    //     />
+    //     <Typography variant="subtitle1">
+    //       <b>{video.snippet.title}</b>
+    //     </Typography>
+    //   </Paper>
+    // </Grid>
   );
 };
 
